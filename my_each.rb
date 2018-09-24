0cb164726ffae32171c1609f137057f130afede1
Paul Fell <swellpf@gmail.com>
2018-09-24 21:51:31 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,9 @@
 require 'pry'
-def my_each(array) {puts} 
+def my_each(array) {puts array[item]} 
   # code here
   i = 0
-  while i < array.size do 
-    yield array[item]
+  while i < array.size do |item|
+    yield 
   i = i + 1
   end
 end
