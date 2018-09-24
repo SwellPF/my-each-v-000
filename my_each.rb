581a53667d9fef574f26632f0e6da3e4e5746974
Paul Fell <swellpf@gmail.com>
2018-09-24 21:35:00 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
+require 'pry'
+def my_each(array) {puts item} # put argument(s) here
   # code here
+  i = 0
+  binding.pry
+  while i < array.size do |item|
+    binding.pry
+    yield
+  i = i + 1
+  end
 end
